0e9ce3089eb5dc64e7ea848350f33678f6bca950
sambelltacq <Sam.Bell@d-tacq.co.uk>
2024-07-18 10:49:16 +0100

    Better start script

--- docker_start.sh
+++ docker_start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+sudo docker rm $(sudo docker ps -a -q) -f
+sudo docker volume prune -f
+sudo docker compose up --build
 
-sudo docker compose build
+
+#sudo docker compose build
 #sudo docker compose build --no-cache
-
-sudo docker compose up
+#sudo docker compose up
